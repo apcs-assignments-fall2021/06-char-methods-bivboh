c663f78cfbb2d264ba350020ea8e838507cbe2ec
athenian <bivboh@gmail.com>
2021-09-20 22:42:41 -0700

    done

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,24 +4,45 @@ public class MyMain {
     // If the char is not an uppercase letter, 
     // it is returned unchanged. 
     public static char toLower(char ch) {
-        // REPLACE WITH YOUR CODE
-        return '!';
+        if (Character.isUpperCase(ch)){
+            ch = Character.toLowerCase(ch);
+            return ch;
+        }
+        else {
+            return ch;
+        }
     }
 
     // Changes a lowercase letter to an uppercase letter
     // If the char is not a lowercase letter,
     // it is returned unchanged.
     public static char toUpper(char ch) {
-        // REPLACE WITH YOUR CODE HERE
-        return '!';
+        if (Character.isLowerCase(ch)){
+            ch = Character.toUpperCase(ch);
+        }
+        else {
+            ch = ch;
+        }
+        return ch;
     }
 
     // The method is given a String as input and returns a
     // new String where all of the numbers have been removed
     // from the original String.
     public static String removeNumbers(String str) {
-        // REPLACE WITH YOUR CODE
-        return "!!!";
+        String new_string = "";
+        for (int i = 0; i < str.length(); i++){
+            char new_char = str.charAt(i);
+            if (new_char > 47 && new_char < 58){
+                new_string = new_string;
+            }
+            else {
+                new_string += new_char;
+
+            }
+
+        }
+        return new_string;
     }
 
     // Write some code to test your methods!
